39586fb68678691971cb244bc68a0492ecca1223
Antonio Bakula <antonio@wem.hr>
2020-06-03 11:52:40 +0200

    AnonGenService - IdentificationNumber2 added, in Croatia this is a Hzzo MBO

--- AnonGenServiceWebRole/IAnonGenService.cs
+++ AnonGenServiceWebRole/IAnonGenService.cs
@@ -9,52 +9,54 @@ using System.Web.Http.Cors;
 
 namespace AnonGenServiceWebRole
 {
-  [ServiceContract]
-  public interface IAnonGenService
-  {
-    [OperationContract]
-    [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GetSupportedCultures")]
-    List<string> GetSupportedCultures();
+	[ServiceContract]
+	public interface IAnonGenService
+	{
+		[OperationContract]
+		[WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GetSupportedCultures")]
+		List<string> GetSupportedCultures();
 
-    [OperationContract]
-    [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GenerateUsers/{culture}/?count={count}")]
+		[OperationContract]
+		[WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "GenerateUsers/{culture}/?count={count}")]
 		[EnableCors(origins: "*", headers: "*", methods: "*")]
 		GenerateUsersResponse GenerateUsers(string culture, int count);
-  }
+	}
 
-  [DataContract]
-  public class User
-  {
-    [DataMember]
-    public string IdentificationNumber { get; set; }
-    [DataMember]
-    public string Firstname { get; set; }
-    [DataMember]
-    public string Lastname { get; set; }
-    [DataMember]
-    public string EMail { get; set; }
-    [DataMember]
-    public string PhoneNumber { get; set; }
-    [DataMember]
-    public string Street { get; set; }
-    [DataMember]
-    public string StreetNumber { get; set; }
-    [DataMember]
-    public string PostalCode { get; set; }
-    [DataMember]
-    public string City { get; set; }
-    [DataMember]
-    public string Country { get; set; }
-  }
+	[DataContract]
+	public class User
+	{
+		[DataMember]
+		public string IdentificationNumber { get; set; }
+		[DataMember]
+		public string IdentificationNumber2 { get; set; }
+		[DataMember]
+		public string Firstname { get; set; }
+		[DataMember]
+		public string Lastname { get; set; }
+		[DataMember]
+		public string EMail { get; set; }
+		[DataMember]
+		public string PhoneNumber { get; set; }
+		[DataMember]
+		public string Street { get; set; }
+		[DataMember]
+		public string StreetNumber { get; set; }
+		[DataMember]
+		public string PostalCode { get; set; }
+		[DataMember]
+		public string City { get; set; }
+		[DataMember]
+		public string Country { get; set; }
+	}
 
-  [DataContract]
-  public class GenerateUsersResponse
-  {
-    [DataMember]
-    public bool Success { get; set; }
-    [DataMember]
-    public List<string> Messages { get; set; } = new List<string>();
-    [DataMember]
-    public List<User> Users { get; set; } = new List<User>();
-  }
+	[DataContract]
+	public class GenerateUsersResponse
+	{
+		[DataMember]
+		public bool Success { get; set; }
+		[DataMember]
+		public List<string> Messages { get; set; } = new List<string>();
+		[DataMember]
+		public List<User> Users { get; set; } = new List<User>();
+	}
 }
